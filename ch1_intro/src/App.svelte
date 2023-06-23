b2c0356af204743577de400d454c64e65bc7d165
“Stuart <liger8268@gmail.com>
2023-06-23 19:22:18 +0900

    upload ch1 intro

---
+++ ch1_intro/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Nested from "./nested.svelte";
+	import Nesquik from "./nesquik.svelte";
+	let name = 'svelte';
+	let src = './images/dutti.png';
+</script>
+
+<h1>Hello {name}!</h1>
+
+<img src={src} alt="smilemates" height="30%"/>
+
+<p>Svelte Tutorials</p>
+<Nested />
+<Nesquik />
+
+
+<style>
+	/* Write your CSS here */
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
---
+++ ch1_intro/src/nested.svelte
@@ -0,0 +1,1 @@
+<p>[nested] Ho~</p>
